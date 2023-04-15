0593533f932183058423071ad19e468ff02ae836
aarcThom <thomas@aarc.io>
2023-04-14 17:20:58 -0700

    Add basic project factory function functionality

--- src/application.js
+++ src/application.js
@@ -1,8 +1,16 @@
-// toDo object factory function
+// shared functions =========================================================================================
+function randomId(givenId) {
+  if (!givenId) {
+    return Math.floor(Math.random()*Date.now()).toString(16);
+  }
+  return givenId;
+};
 
-const ToDoFactory = (tdComplete = false, tdTitle = 'New Task', tdDescription = 'Task Description',
+// toDo object factory function =============================================================================
+const ToDoFactory = (tdId, tdComplete = false, tdTitle = 'New Task', tdDescription = 'Task Description',
   tdDueDate = '12-12-12', tdPriority = 'low') => {
-
+  
+  const id = randomId(tdId);
   let complete = tdComplete;
   let title = tdTitle;
   let description = tdDescription;
@@ -38,15 +46,40 @@ const ToDoFactory = (tdComplete = false, tdTitle = 'New Task', tdDescription = '
   };
 
   // getter for all values
-  const getValues = () => ({ complete, title, description, dueDate, priority });
+  const getValues = () => ({ id, complete, title, description, dueDate, priority });
 
   return { changeStatus, changeText, changeDate, changePriority, getValues };
 };
 
+// projects factory function ================================================================================
 
+const ProjectsFactory = (projectId, projectTitle = 'New Project', 
+  projectDescription = 'Project Description') => {
+  
+  const id = randomId(projectId);
+  let title = projectTitle;
+  let description = projectDescription;
+  const toDoList = [];
 
+  // title and description setter
+  const changeText = (textToChange, newChange) => {
+    if (textToChange === 'title') {
+      title = newChange; } else {
+      description = newChange;
+    }
+  };
 
-const test = ToDoFactory();
+  // add toDos to the list
+  const addTodo = () => toDoList.push(ToDoFactory());
+
+  // getter for all values
+  const getValues = () => ({ id, title, description, toDoList});
+
+  return { changeText, getValues, addTodo};
+
+};
+
+const test = ProjectsFactory();
 console.log(test.getValues());
-test.changeStatus();
+test.addTodo();
 console.log(test.getValues());
