79fcf722556be524230342aec7a099f6f88806bd
aarcThom <thomas@aarc.io>
2023-04-26 22:08:12 -0700

    Start figuring out packery

--- src/dom.js
+++ src/dom.js
@@ -4,10 +4,22 @@ import './style.css';
 const canvas = document.querySelector('#canvas');
 
 // dom elements ===========================
-function projectDomFactory (projName, projDesc) {
+
+function toDoDom (hostProj,tdName, tdId) {
+  const newToDo = document.createElement('div');
+  newToDo.className = 'todo-collapsed';
+  newToDo.id = tdId;
+  newToDo.textContent = tdName;
+
+  const host = document.querySelector(`#${hostProj}`);
+  host.appendChild(newToDo);
+};
+
+function projectDom (proj, projName, projDesc, projId, addTDLogic) {
   const newProj = document.createElement('div');
   newProj.className = 'project-collapsed';
-
+  newProj.id = projId;
+  
   const projHeader = document.createElement('div');
   projHeader.className = 'project-header';
   projHeader.textContent = projName;
@@ -18,18 +30,51 @@ function projectDomFactory (projName, projDesc) {
   projDescription.textContent = projDesc;
   newProj.appendChild(projDescription);
 
+  // if we are organizing by project
+  if (proj) {
+    const addToDoBox = document.createElement('div');
+    addToDoBox.className = 'project-add-todo';
+
+    const addToDoBut = document.createElement('button');
+    addToDoBut.textContent = 'Add a new task';
+    addToDoBut.className = 'project-add-todo-button';
+
+    addToDoBut.addEventListener('click', () => {
+      // I pass in a function from the index.js file in order to reference the main page object
+      const newTD = addTDLogic(proj);
+      toDoDom(projId, newTD.getTitle(), newTD.getId());
+    });
+
+    addToDoBox.appendChild(addToDoBut);
+    newProj.appendChild(addToDoBox);
+  };
+
   return newProj;
 };
- 
 
 // rendering the projects ================================================================================
-function renderProjects (groupByProj, projects, toDos) {
+function renderProjects (groupByProj, projects, toDos, tdLogicFunc) {
   canvas.innerHTML=''; // clear the canvas
 
-  projects.forEach( prj => {
-    const newP = projectDomFactory(prj.getId(), prj.getDescription());
+  if (groupByProj) {
+    projects.forEach( prj => {
+      const newP = projectDom(prj, prj.getId(), prj.getDescription(), prj.getId(), tdLogicFunc);
+      canvas.appendChild(newP);
+    });
+  
+    toDos.forEach( td => {
+      toDoDom(td.getProject(), td.getTitle(), td.getId());
+    });
+  }
+  else {
+    const newP = projectDom(false, 'null', 'all projects', 'null', tdLogicFunc );
     canvas.appendChild(newP);
-  });
+
+    toDos.forEach( td => {
+      toDoDom('null', td.getTitle(), td.getId());
+    });
+  }
+  
 };
 
 
--- src/index.js
+++ src/index.js
@@ -2,55 +2,44 @@ import './style.css';
 import { toDoList } from "./application";
 import { renderProjects } from './dom';
 
+const Packery = require('packery');
+
+// the packery libary
+const packeryCanvas = document.querySelector('#canvas');
+const pckry = new Packery( packeryCanvas, {
+  itemSelector: '.project-collapsed',
+  gutter: 20
+});
 
 // the toDo list data
 const page = toDoList;
 const pgProjects = page.getProjects();
 const pgToDos = page.getToDos();
-const groupMode = page.getGroupByProject();
+let groupMode = page.getGroupByProject();
 
 // the page layout elements
 const canvas = document.querySelector('#canvas');
-canvas.innerHTML = 'hellllllllllllllllllllllo';
 
-
-page.addProject();
-page.addProject();
-page.addProject();
-
-
-const newToDo1 = page.addToDo(pgProjects[0]);
-newToDo1.setTitle('banana');
-newToDo1.setDueDate('01-02-03');
-newToDo1.setPriority(9);
-const newToDo2 = page.addToDo(pgProjects[0]);
-newToDo2.setDueDate('99-02-03');
-newToDo2.setPriority(1);
-newToDo2.setTitle('apple');
-const newToDo3 = page.addToDo(pgProjects[0]);
-newToDo3.setDueDate('99-99-99');
-newToDo3.setPriority(2);
-newToDo3.setTitle('tomato');
-const newToDo4 = page.addToDo(pgProjects[0]);
-newToDo4.setDueDate('07-02-03');
-newToDo4.setPriority(6);
-newToDo4.setTitle('watermelon');
-
-
-
-page.setSortMode(3);
-
-page.sortToDos();
-
-pgToDos.forEach(obj => console.log(obj.getTitle()));
-
+// the add toDo button that I pass through to the dom
+function newToDo (hostPrj) {
+  return page.addToDo(hostPrj);
+};
 
 // the add project button
 const addProjBut = document.querySelector('#add-project-but');
 addProjBut.addEventListener('click', () => {
   page.addProject();
   console.log(page.getProjects());
-  renderProjects(groupMode, pgProjects, pgToDos);
+  renderProjects(groupMode, pgProjects, pgToDos, newToDo);
 })
+
+// the group mode button
+const groupModeBut = document.querySelector('#group-mode-but');
+groupModeBut.addEventListener('click', () => {
+  page.toggleGroupByProject();
+  groupMode = page.getGroupByProject();
+  renderProjects(groupMode, pgProjects, pgToDos, newToDo);
+})
+
 
 
