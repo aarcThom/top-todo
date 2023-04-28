243659eb6821bdbf02e912c995ca0d6ae83fbffd
aarcThom <thomas@aarc.io>
2023-04-27 23:27:09 -0700

    Add Dragabilly to page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import './style.css';
 import Packery from 'packery';
+import Draggabilly from 'draggabilly';
 import { toDoList } from "./application";
 import { renderProjects } from './dom';
-import { doc } from 'prettier';
 
 
-// the packery libary
+// the packery initializing
 const pckry = new Packery( '#canvas', {
   itemSelector: '.project-collapsed',
   gutter: 20,
-  percentPosition: true,
 });
+
+// initial Draggabilly binding
+pckry.getItemElements().forEach(element => {
+  const draggie = new Draggabilly(element);
+  pckry.bindDraggabillyEvents(draggie);
+});
+
 
 // the toDo list data
 const page = toDoList;
@@ -29,10 +35,18 @@ function newToDo (hostPrj) {
 // the add project button
 const addProjBut = document.querySelector('#add-project-but');
 addProjBut.addEventListener('click', () => {
+
+  // you can't clear the DOM, just add 1 new item
   const newObj = document.createElement('div');
   newObj.className = 'project-collapsed';
   canvas.appendChild(newObj);
+  
+  // append the item to the packery instance
   pckry.appended( newObj );
+
+  // bind the dragabilly event to packery
+  const newDrag = new Draggabilly(newObj);
+  pckry.bindDraggabillyEvents(newDrag);
 })
 
 // the group mode button
